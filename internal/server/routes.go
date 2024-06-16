49feafbff67343bf57de28b0a250ab0629977aa2
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2024-06-16 22:25:20 +0200

    fix

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -13,7 +13,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	e.Use(middleware.Recover())
 	CORSConfig := middleware.CORSConfig{
 		Skipper:          middleware.DefaultSkipper,
-		AllowOrigins:     []string{"https://localhost:5173", "http://localhost:5173", "http://localhost:4173", "https://api.hudori.app"},
+		AllowOrigins:     []string{"https://localhost:5173", "http://localhost:5173", "http://localhost:4173", "https://api.hudori.app", "https://hudori.app"},
 		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
 		AllowCredentials: true,
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderSetCookie, echo.HeaderCookie, echo.HeaderContentType, echo.HeaderAccept},

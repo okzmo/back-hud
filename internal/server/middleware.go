c15de95bfad3d38ff9064b8a3e44e260b5aa0d31
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2024-06-20 12:06:19 +0200

    fix: http code

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -8,22 +8,22 @@ func (s *Server) SessionAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		sessionCookie, err := c.Cookie("session")
 		if err != nil {
-			return echo.NewHTTPError(401, "No session cookie available")
+			return echo.NewHTTPError(404, "No session cookie available")
 		}
 
 		sess, err := s.db.GetSession(sessionCookie.Value)
 		if err != nil {
-			return echo.NewHTTPError(401, "Invalid session")
+			return echo.NewHTTPError(403, "Invalid session")
 		}
 
 		if sess.UserAgent != c.Request().Header.Get("X-User-Agent") {
-			return echo.NewHTTPError(401, "Invalid session")
+			return echo.NewHTTPError(403, "Invalid session")
 		}
 
 		userId := c.Request().Header.Get("X-User-ID")
 
 		if userId != "" && userId != sess.UserId {
-			return echo.NewHTTPError(401, "Invalid session")
+			return echo.NewHTTPError(403, "Invalid session")
 		}
 
 		return next(c)
